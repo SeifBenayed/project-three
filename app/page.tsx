587fec77e78442f0ff37c58396615a22d9086e4c
astrosbd <Seifeddine.ben.ayed@efrei.net>
2024-04-30 12:31:10 +0200

    V 1 fix template

--- app/page.tsx
+++ app/page.tsx
@@ -8,23 +8,7 @@ export default function Chat() {
 
     return (
         <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
-            {messages.length > 0
-                ? messages.map((m) => (
-                    <div key={m.id} className="whitespace-pre-wrap">
-                        {m.role === "user" ? "User: " : "AI: "}
-                        <ReactMarkdown>{m.content}</ReactMarkdown>
-                    </div>
-                ))
-                : null}
-
-            <form onSubmit={handleSubmit}>
-                <input
-                    className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
-                    value={input}
-                    placeholder="Say something..."
-                    onChange={handleInputChange}
-                />
-            </form>
+         Welcome to hedi
         </div>
     );
 }

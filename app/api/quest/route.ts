737c70f891d38ce0594070f02654b7c12f960fd9
astrosbd <Seifeddine.ben.ayed@efrei.net>
2024-05-29 16:21:19 +0200

    v 1.9 Query fix

--- app/api/quest/route.ts
+++ app/api/quest/route.ts
@@ -7,19 +7,14 @@ export async function POST(req: Request) {
     const collectionName = "user_query";
     const collection_ingredient = client.db(dbName).collection(collectionName);
         // Get the list of ingredients from the request body as a plain text string
-        const  body  = await req.text();
-
-    const headers = JSON.stringify(req.headers);
-    const IP =  JSON.stringify(req.ip);
-    const agent = req.headers["user-agent"];
-    const language =  req.headers["accept-language"];
+    const  body  = await req.text();
 
     if (!body) {
             throw new Error('Missing body in request body');
         }
 
         // Insert the request body into the database
-    const result = await collection_ingredient.insertOne({ body, createdAt: new Date(),headers, IP, agent, language, country });
+    const result = await collection_ingredient.insertOne({ body, createdAt: new Date() });
 
         // Return the result of the insertion
     return new Response(JSON.stringify({ insertedId: result.insertedId }), {
